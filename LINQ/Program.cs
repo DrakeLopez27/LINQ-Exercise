acc5c6cb429519c5e2ecab1b328072c6f619a446
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-02-21 15:01:48 -0600

    ex1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var VideoGames = new List<string>() { "Phasmophobia", "Call of Duty", "Howgwart's Legacy" };
+
+
+
+            var containsSpace = VideoGames.Where(x => x.Contains(" ")).ToList();
+            foreach (var videoGame in containsSpace)
+            {
+                Console.WriteLine(videoGame);
+            }
+
+
+            var count = VideoGames.Count();
+            Console.WriteLine(count);
+
+            
+           var LengthOrder = VideoGames.OrderByDescending(x => x.Length).ToList();
+            for (int i = 0; i < LengthOrder.Count; i++)
+            {
+                Console.WriteLine(LengthOrder[i]);
+            }
         }
     }
 }
